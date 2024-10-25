0516f99fcf0ffcd1226beb4681c990248d85241b
nfmmendes <nilson.fmmendes@outlook.com>
2024-10-25 16:46:54 +0200

    Refactor AbtractEnum

--- Common/AbstractEnum.cs
+++ Common/AbstractEnum.cs
@@ -6,23 +6,11 @@ using System.Threading.Tasks;
 
 namespace Common
 {
-    public abstract class AbstractEnum
+    public abstract class AbstractEnum(string label, string dbCode)
     {
         public List<AbstractEnum> EnumList { get; protected set; }
         public string Label { get; protected set; }
-        public string DbCode { get; protected set; }
-
-        protected AbstractEnum(string label, string dbCode){
-            Label = label;
-            DbCode = dbCode;
-        } 
-
-        public AbstractEnum()
-        {
-            
-        }
-
-
+        public readonly string DbCode = dbCode;
 
         public override string ToString()
         {
