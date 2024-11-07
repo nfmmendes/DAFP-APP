96a80475f186801b5695d4aeeca44f068eede06d
nfmmendes <nilson.fmmendes@outlook.com>
2024-11-07 17:10:26 +0100

    Implement function ValidateArrivalAfterDeparture

--- Solver/SolutionValidator.cs
+++ Solver/SolutionValidator.cs
@@ -1,5 +1,7 @@
-﻿using Solver.SolutionData;
+﻿using Common;
+using Solver.SolutionData;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Solver
 {
@@ -14,7 +16,19 @@ namespace Solver
 
         public bool Validate(GeneralSolution solution, SolverInput input)
         {
-            return true;
+            return ValidateArrivalAfterDeparture(solution);
+        }
+
+        private bool ValidateArrivalAfterDeparture(GeneralSolution solution) { 
+            var invalidFlights = solution.Flights.Where(x => x.DepartureTime > x.ArrivalTime).ToList();
+
+            if (invalidFlights.None())
+                return true;
+
+            Errors.AddRange(invalidFlights.Select(x => new string($"The flight with id {x.Id}, from {x.Origin} to {x.Destination} arrives" +
+                $"before the departure time")).ToList());
+
+            return false;
         }
     }
 }
