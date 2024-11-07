cce5e4e3ca7fff2aec92ac9482feda7fc01660f0
nfmmendes <nilson.fmmendes@outlook.com>
2024-11-07 16:43:51 +0100

    Create class SolutionValidator

---
+++ Solver/SolutionValidator.cs
@@ -0,0 +1,20 @@
+﻿using Solver.SolutionData;
+using System.Collections.Generic;
+
+namespace Solver
+{
+    internal class SolutionValidator
+    {
+        public List<string> Errors { get; private set; }
+
+        SolutionValidator()
+        {
+            Errors = new List<string>();
+        }
+
+        public bool Validate(GeneralSolution solution, SolverInput input)
+        {
+            return true;
+        }
+    }
+}
