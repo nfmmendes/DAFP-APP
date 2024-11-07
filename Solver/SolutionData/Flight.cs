8d56fac3238da4bb7d6c63dcc19554bf25cdb446
nfmmendes <nilson.fmmendes@outlook.com>
2024-11-07 16:56:03 +0100

    Refactor fuel properties on Flight class

--- Solver/SolutionData/Flight.cs
+++ Solver/SolutionData/Flight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ICSharpCode.SharpZipLib;
 using SolverClientComunication.Models;
 using Aiport = SolverClientComunication.Models.DbAirport;
 using Passenger = SolverClientComunication.Models.DbRequest;
@@ -8,6 +9,9 @@ namespace SolutionData
 {
     public class Flight{
 
+        private double fuelOnTakeOff;
+        private double fuelOnLanding;
+
         public long Id { get; private set; }
         private static long LastId = 0;
         public DbAirplane Airplanes { get; set; }
@@ -15,8 +19,33 @@ namespace SolutionData
         public Aiport Destination { get; set; }
         public TimeSpan DepartureTime { get; set; }
         public TimeSpan ArrivalTime { get; set; }
-        public double FuelOnTakeOff { get; set; }
-        public double FuelOnLanding { get; set; }
+        public double FuelOnTakeOff
+        {
+            get
+            {
+                return fuelOnTakeOff;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("It is not allowed to set a negative value to this property");
+                fuelOnTakeOff = value;
+            }
+        }
+
+        public double FuelOnLanding {
+            get {
+                return fuelOnLanding;
+            }
+
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("It is not allowed to set a negative value to this property");
+
+                value = fuelOnLanding;
+            }
+        }
         public List<Passenger> Passengers { get; set;  }
         public bool IsFull => Passengers.Count >= Airplanes.Capacity;
 
