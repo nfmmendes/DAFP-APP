def1bd0030e93ebffb609a891eee850a0c2c328e
nfmmendes <nilson.fmmendes@outlook.com>
2024-11-13 12:49:39 +0100

    Create the enum RequestColumnsEnum

---
+++ SolverClientComunication/Enums/Excel/RequestColumnsEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace SolverClientComunication.Enums.Excel
+{
+    public enum RequestColumnsEnum
+    {
+        Name,
+        PNR,
+        Sex,
+        Class,
+        IsChildren,
+        Origin,
+        Destination,
+        DepartureTimeWindowBegin,
+        DepartureTimeWindowEnd,
+        ArrivalTimeWindowBegin,
+        ArrivalTimeWindowEnd
+    }
+}
