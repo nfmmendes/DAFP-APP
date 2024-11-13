adc2907eeb21eac35c6ae1cf8b14ffa8c648ab8d
nfmmendes <nilson.fmmendes@outlook.com>
2024-11-13 12:02:28 +0100

    Create enum AirportColumnsEnum

---
+++ SolverClientComunication/Enums/Excel/AirportColumnsEnum.cs
@@ -0,0 +1,24 @@
+﻿using SolverClientComunication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverClientComunication.Enums.Excel
+{
+    public enum AirportColumnsEnum
+    {
+        AiportName,
+        IataCode,
+        Latitude,
+        Longitude,
+        Elevation,
+        RunwayLength,
+        Region,
+        Mtow_Ape3,
+        Mtow_pc12,
+        GroundTime,
+        LandingCost
+    }
+}
