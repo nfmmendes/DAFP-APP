5563813a30547373156ccf748ef051f305829de1
nfmmendes <nilson.fmmendes@outlook.com>
2024-11-14 19:37:33 +0100

    Create AirplaneColumnEnum

---
+++ SolverClientComunication/Enums/Excel/AirplaneColumnsEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverClientComunication.Enums.Excel
+{
+    public enum AirplaneColumnsEnum
+    {
+        Model,
+        Prefix,
+        Range,
+        Weight,
+        MaxWeight,
+        CruiseSpeed,
+        Capacity,
+        BaseAirport,
+        FuelConsumptionFirstHour,
+        FuelConsumptionSecondHour,
+        MaxFuel
+    }
+}
